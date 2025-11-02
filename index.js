a962a78ac7c6869e9505c937ea0bb65730acc0e2
Pedro Felipe <pedrofelipeot@gmail.com>
2025-11-02 10:40:10 -0300

    logs

--- index.js
+++ index.js
@@ -137,3 +137,5 @@ app.post("/esp/event", async (req, res) => {
 // =======================
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+console.log(serviceAccount.private_key);
+
